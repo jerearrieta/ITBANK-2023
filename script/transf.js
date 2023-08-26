153ad21dc5df1a4d27309324313828d6e04fdf4e
jerearrieta <jerearrieta2254@gmail.com>
2023-08-25 23:12:55 -0300

    transferencia terminada

--- script/transf.js
+++ script/transf.js
@@ -1,10 +1,13 @@
 let formulario = document.getElementById("form-transf");
 let btnTransf = document.getElementById("boton-transf");
-let montoDisponible = 100000;
-let montoHTML = document.getElementById('saldo-disponible');
+let saldoDisponible = document.getElementById("saldo-disponible");
+let montoDisponible = sessionStorage.getItem('montoDisponible') || 100000;
+if (saldoDisponible) {
+  saldoDisponible.textContent = montoDisponible;
+}
 
 
-btnTransf.addEventListener("click", () => {
+btnTransf.addEventListener("click", (e) => {
   let cbu = document.getElementById("cbu").value;
   let montoTransf = parseFloat(document.getElementById("monto-transf").value);
   let motivo = document.getElementById("motivo").value;
@@ -26,14 +29,16 @@ btnTransf.addEventListener("click", () => {
     mostrarMensaje(`Dinero insuficiente, saldo disponible $${montoDisponible}`);
     e.preventDefault();
   } else {
-    montoDisponible -= montoTransf;
+    sessionStorage.setItem('montoDisponible', montoDisponible - montoTransf);
     mostrarMensaje(
-      `Transferencia exitosa, tu monto disponible es de: $${montoDisponible}`
+      `Transferencia exitosa, tu monto disponible es de: $${sessionStorage.getItem('montoDisponible')}`
     );
     e.preventDefault();
   }
 
   return montoDisponible
-  
+
 });
+
+
 
