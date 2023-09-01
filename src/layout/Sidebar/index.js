9fa89990cf324468513bf0d0a8e4939998a4e09c
Camila González <109551369+c-caam@users.noreply.github.com>
2023-08-31 22:03:58 -0300

    agregar componentes de layout

--- src/layout/Sidebar/index.js
+++ src/layout/Sidebar/index.js
@@ -3,30 +3,30 @@ import './style.css';
 export default function Header() {
     return (
         <aside>
-            <a href="inicio.html" class="sidebar-option selected">
-                <img class="sidebar-option-icon" src="img/home.png" />
+            <a href="inicio.html" className="sidebar-option selected">
+                <img className="sidebar-option-icon" src="img/home.png" alt="home icono" />
                     <p>Inicio</p>
             </a>
-            <a href="cuentas.html" class="sidebar-option">
-                <img class="sidebar-option-icon" src="img/user.png" />
+            <a href="cuentas.html" className="sidebar-option">
+                <img className="sidebar-option-icon" src="img/user.png" alt="cuentas icono" />
                     <p>Cuentas</p>
             </a>
-            <a href="transferencias.html" class="sidebar-option">
-                <img class="sidebar-option-icon" src="img/transaction.png" />
+            <a href="transferencias.html" className="sidebar-option" alt="transferencias icono">
+                <img className="sidebar-option-icon" src="img/transaction.png" />
                     <p>Transferencias</p>
                     <div>
-                        <img class="sidebar-option-arrow" src="img/arrow-down-white.png" />
+                        <img className="sidebar-option-arrow" src="img/arrow-down-white.png" alt="icono flecha" />
                     </div>
             </a>
-            <a href="prestamos.html" class="sidebar-option">
-                <img class="sidebar-option-icon" src="img/personal.png" />
+            <a href="prestamos.html" className="sidebar-option">
+                <img className="sidebar-option-icon" src="img/personal.png" alt="icono préstamos" />
                     <p>Prestamos</p>
                     <div>
-                        <img class="sidebar-option-arrow" src="img/arrow-down-white.png" />
+                        <img className="sidebar-option-arrow" src="img/arrow-down-white.png" alt="icono flecha" />
                     </div>
             </a>
-            <a href="index.html" class="sidebar-option">
-                <img class="sidebar-option-icon" src="img/log-out.png" />
+            <a href="index.html" className="sidebar-option">
+                <img className="sidebar-option-icon" src="img/log-out.png" alt="icono cerrar sesión" />
                     <p>Cerrar sesion</p>
             </a>
         </aside>
