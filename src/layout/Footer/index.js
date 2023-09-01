279a72e1eceb8f08a08dbc7b2d7990225934baf3
Camila González <109551369+c-caam@users.noreply.github.com>
2023-08-31 22:04:50 -0300

    agregar componentes de layout

--- src/layout/Footer/index.js
+++ src/layout/Footer/index.js
@@ -3,32 +3,32 @@ import './style.css';
 export default function Header() {
     return (
         <footer>
-            <div class="footer-logo">
-                <a href="inicio.html"><img class="logo" src="img/logo.png" alt="ITBA" /></a>
+            <div className="footer-logo">
+                <a href="inicio.html"><img className="logo" src="img/logo.png" alt="ITBA" /></a>
             </div>
 
-            <div class="footer-info">
-                <div class="contact-info">
+            <div className="footer-info">
+                <div className="contact-info">
                     <p>example@itbank.com</p>
                     <p>11 1234-5678 / 11 1234-5678</p>
                     <p>Domicilio 1234</p>
                 </div>
-                <div class="legal-info">
+                <div className="legal-info">
                     <a href="#">Terminos y condiciones</a>
                     <a href="#">Politicas de privacidad</a>
                     <p>© 2023 ITBANK</p>
                 </div>
-                <div class="social-media">
+                <div className="social-media">
                     <div>
-                        <a href="https://www.facebook.com/"><img class="logo" src="img/facebook.png" alt="ITBA" /></a>
+                        <a href="https://www.facebook.com/"><img className="logo" src="img/facebook.png" alt="logo de facebook" /></a>
                         <a href="https://www.facebook.com/"><p>/itbank</p></a>
                     </div>
                     <div>
-                        <a href="https://www.twitter.com/"><img class="logo" src="img/twitter.png" alt="ITBA" /></a>
+                        <a href="https://www.twitter.com/"><img className="logo" src="img/twitter.png" alt="logo de twitter" /></a>
                         <a href="https://www.twitter.com/"><p>@itbank</p></a>
                     </div>
                     <div>
-                        <a href="https://www.instagram.com/"><img class="logo" src="img/instagram.png" alt="ITBA" /></a>
+                        <a href="https://www.instagram.com/"><img className="logo" src="img/instagram.png" alt="logo de instagram" /></a>
                         <a href="https://www.instagram.com/"><p>@itbank</p></a>
                     </div>
                 </div>
