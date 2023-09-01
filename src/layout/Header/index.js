d19086d89cc45dd07ced2062775b1b9836563b1b
Camila González <109551369+c-caam@users.noreply.github.com>
2023-08-31 22:04:27 -0300

    agregar componentes de layout

--- src/layout/Header/index.js
+++ src/layout/Header/index.js
@@ -3,11 +3,11 @@ import './style.css';
 export default function Header () {
     return (
         <header>
-            <div class="header-sidebar">
+            <div className="header-sidebar">
                 <a href="inicio.html"><img src="img/white-logo.png" alt="ITBA" /></a>
             </div>
 
-            <div class="header-content">
+            <div className="header-content">
                 <a href="#">Atencion al cliente</a>
                 <a href="#">Notificaciones</a>
                 <select id="lang-selector">
