a08e6c8febaee8d80aa7f97f8c09d2ee97dea4cf
Camila González <109551369+c-caam@users.noreply.github.com>
2023-09-05 19:13:17 -0300

    agregar className faltantes

--- src/views/Calculadora/index.js
+++ src/views/Calculadora/index.js
@@ -3,43 +3,43 @@ import './style.css';
 export default function Calculadora() {
     return (
         <>
-            <div class="loan-calculator">
+            <div className="loan-calculator">
                 <h1>Calcular prestamos</h1>
-                <div class="loan-calculator-box">
-                    <div class="loan-calculator-field-box">
+                <div className="loan-calculator-box">
+                    <div className="loan-calculator-field-box">
                         <p>Importe</p>
-                        <input type="number" id="amount-input" class="field" />
+                        <input type="number" id="amount-input" className="field" />
                     </div>
-                    <div class="loan-calculator-field-box">
+                    <div className="loan-calculator-field-box">
                         <p>Interés</p>
-                        <input type="number" id="interest-input" class="field" />
+                        <input type="number" id="interest-input" className="field" />
                     </div>
-                    <div class="loan-calculator-field-box">
+                    <div className="loan-calculator-field-box">
                         <p>Plazo de amortización</p>
-                        <div class="time-field-box">
-                            <select id="time-selector" class="time-selector">
+                        <div className="time-field-box">
+                            <select id="time-selector" className="time-selector">
                                 <option value="mes">Meses</option>
                                 <option value="año">Años</option>
                             </select>
-                            <input type="number" id="time-input" class="time-input" />
+                            <input type="number" id="time-input" className="time-input" />
                         </div>
                     </div>
-                    <div class="loan-calculator-button-box">
-                        <button id="loan-calculator-button" class="loan-calculator-button">Calcular</button>
+                    <div className="loan-calculator-button-box">
+                        <button id="loan-calculator-button" className="loan-calculator-button">Calcular</button>
                     </div>
                 </div>
             </div>
 
-            <div class="loan-calculator">
+            <div className="loan-calculator">
                 <h2>Resultados</h2>
-                <div class="loan-calculator-box">
-                    <div class="loan-calculator-field-box">
+                <div className="loan-calculator-box">
+                    <div className="loan-calculator-field-box">
                         <p>Cuota mensual</p>
-                        <input type="text" id="monthly-payment" class="field" disabled />
+                        <input type="text" id="monthly-payment" className="field" disabled />
                     </div>
-                    <div class="loan-calculator-field-box">
+                    <div className="loan-calculator-field-box">
                         <p>Total intereses</p>
-                        <input type="text" id="total-interest" class="field" disabled />
+                        <input type="text" id="total-interest" className="field" disabled />
                     </div>
                 </div>
             </div>
