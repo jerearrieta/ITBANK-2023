55c786bf8109b0d6342d3e2311423aa1750eec58
Camila González <109551369+c-caam@users.noreply.github.com>
2023-09-05 19:14:49 -0300

    agregar className faltantes

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -3,42 +3,42 @@ import './style.css';
 export default function Home() {
     return (
         <>
-            <div class="welcome-user">
+            <div className="welcome-user">
                 <h1>Hola, Usuario</h1>
             </div>
             <hr />
-            <div class="account-status">
-                <p class="account-status-label">Estado de tu cuenta</p>
-                <div class="account-status-box">
-                    <p class="available-money-label">Pesos disponibles</p>
-                    <p id="saldo-disponible" class="available-money-pesos"></p>
-                    <p class="available-money-label">USD disponible</p>
-                    <p id="usd-disponible" class="available-money-pesos"></p>
+            <div className="account-status">
+                <p className="account-status-label">Estado de tu cuenta</p>
+                <div className="account-status-box">
+                    <p className="available-money-label">Pesos disponibles</p>
+                    <p id="saldo-disponible" className="available-money-pesos"></p>
+                    <p className="available-money-label">USD disponible</p>
+                    <p id="usd-disponible" className="available-money-pesos"></p>
                 </div>
             </div>
             <hr />
-            <div class="recent-activity">
-                <p class="recent-activity-label">Actividad reciente</p>
-                <div class="recent-activity-box">
-                    <div class="recent-activity-entry">
-                        <div class="recent-activity-entry-part">
-                            <p class="recent-activity-entry-title">Transferencia enviada</p>
-                            <p class="recent-activity-entry-value">-$10.000</p>
+            <div className="recent-activity">
+                <p className="recent-activity-label">Actividad reciente</p>
+                <div className="recent-activity-box">
+                    <div className="recent-activity-entry">
+                        <div className="recent-activity-entry-part">
+                            <p className="recent-activity-entry-title">Transferencia enviada</p>
+                            <p className="recent-activity-entry-value">-$10.000</p>
                         </div>
-                        <div class="recent-activity-entry-part">
-                            <p class="recent-activity-entry-third">Google</p>
-                            <p class="recent-activity-entry-date">20/8/2023</p>
+                        <div className="recent-activity-entry-part">
+                            <p className="recent-activity-entry-third">Google</p>
+                            <p className="recent-activity-entry-date">20/8/2023</p>
                         </div>
                     </div>
                     <hr />
-                    <div class="recent-activity-entry">
-                        <div class="recent-activity-entry-part">
-                            <p class="recent-activity-entry-title">Transferencia enviada</p>
-                            <p class="recent-activity-entry-value">-$10.000</p>
+                    <div className="recent-activity-entry">
+                        <div className="recent-activity-entry-part">
+                            <p className="recent-activity-entry-title">Transferencia enviada</p>
+                            <p className="recent-activity-entry-value">-$10.000</p>
                         </div>
-                        <div class="recent-activity-entry-part">
-                            <p class="recent-activity-entry-third">Google</p>
-                            <p class="recent-activity-entry-date">20/8/2023</p>
+                        <div className="recent-activity-entry-part">
+                            <p className="recent-activity-entry-third">Google</p>
+                            <p className="recent-activity-entry-date">20/8/2023</p>
                         </div>
                     </div>
                 </div>
