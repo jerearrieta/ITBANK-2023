30f5050ea27681fc587ec6200be5e912fc0e13ca
Gabriel Checchi <checchigab@gmail.com>
2023-09-20 19:25:59 -0300

    Unificar Login y Registro

--- app/page.js
+++ app/page.js
@@ -1,44 +1,28 @@
+"use client"
+
+import { useState } from "react";
+
+import FormLogin from "./auth/FormLogin";
+import FormRegistro from "./auth/FormRegistro";
 import Footer from "./(dashboards)/components/Footer"
 
-export default function Home() {
+
+export default function Index() {
+  const [inLogin, setInLogin] = useState(true);
+
   return (
     <>
-    <div className="bg-gradient-to-tr from-blue-900 to-slate-700 min-h-screen">
-      <header className="py-12">
-        <img src="/white_logo_title.png" className="w-[420px] h-auto mx-auto" />
-      </header>
-      <main className="flex flex-col items-center bg-inherit p-12">
+      <div className="min-h-screen bg-gradient-to-tr from-blue-900 to-slate-700">
+        <header className="p-12">
+          <img src="/white_logo_title.png" className="w-[420px] h-auto mx-auto" />
+        </header>
 
-        <form className="flex flex-col gap-10 p-8 rounded-2xl bg-gray-300 bg-opacity-60">
-          <div className="flex flex-col items-center">
-            <h1 className="text-3xl font-semibold">Bienvenido al Guardian Bank</h1>
-            <p className="text-sm">Inicia sesion para acceder a su cuenta</p>
-          </div>
-          <div className="form-group gap-5">
-            <div className="form-field">
-              <label className="form-label text-black">Email</label>
-              <input placeholder="Introduzca su email aqui" type="email" className="input max-w-full border-[1px] bg-white border-gray-500" />
-            </div>
-            <div className="form-field">
-              <label className="form-label text-black">Clave</label>
-              <input placeholder="Introduzca su clave aqui" type="password" className="input max-w-full border-[1px] bg-white border-gray-500" />
-            </div>
-            <div className="form-field pt-5">
-              <div className="form-control justify-between">
-                <button type="submit" className="btn btn-primary w-full">Iniciar Sesion</button>
-              </div>
-            </div>
+        <main className="items-center bg-inherit p-12">
+          { inLogin ? <FormLogin setInLogin={setInLogin} /> : <FormRegistro setInLogin={setInLogin} /> }
+        </main>
+      </div>
 
-            <div className="form-field">
-              <div className="form-control justify-center">
-                <a href="login" className="link link-underline-hover link-primary text-sm">¿Ya tiene una cuenta? Inicie Sesion.</a>
-              </div>
-            </div>
-          </div>
-        </form>
-      </main>
-    </div>
-    <Footer></Footer>
+      <Footer />
     </>
   )
 }
