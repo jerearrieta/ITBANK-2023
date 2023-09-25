77fb35770fe9f1a4d20436237bb50df8f05578a6
jerearrieta <jerearrieta2254@gmail.com>
2023-09-25 13:57:37 -0300

    estilizando usuarios

--- app/(dashboards)/usuarios/[id]/page.js
+++ app/(dashboards)/usuarios/[id]/page.js
@@ -9,7 +9,7 @@ export const dynamicParams = false;
 // generateStaticParams es el equivalente a getStaticPaths del Pagers Router
 export async function generateStaticParams() {
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-    const { data } = await supabase.from("users").select("id, dni, first_name, last_name");
+    const { data } = await supabase.from("users").select("id, dni, first_name, last_name, birth, sex");
 
     return data.map((record) => ({
         id: String(record.id)
@@ -18,16 +18,18 @@ export async function generateStaticParams() {
 
 export default async function UserDetail({ params }) {
     const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-    const { data } = await supabase.from("users").select("dni, first_name, last_name").eq("id", params.id).single();
+    const { data } = await supabase.from("users").select("dni, first_name, last_name, birth, sex").eq("id", params.id).single();
 
     return (
-        <div>
-            <p>{params.id}</p>
-            <p>{data.dni}</p>
-            <p>{data.first_name}</p>
-            <p>{data.last_name}</p>
+        <div className="flex flex-col w-3/4 mx-auto rounded-2xl shadow-md bg-gray-300 p-3">
+            <p className="border-b py-2 font-bold">Id: {params.id}</p>
+            <p className="border-b py-2 font-bold">Dni: {data.dni}</p>
+            <p className="border-b py-2 font-bold">Nombre: {data.first_name}</p>
+            <p className="border-b py-2 font-bold">Apellido: {data.last_name}</p>
+            <p className="border-b py-2 font-bold">Nacimiento: {data.birth}</p>
+            <p className="border-b py-2 font-bold">Sexo: {data.sex}</p>
 
-            <Link href={`/transferencias/${params.id}`}>Transferir</Link>
+            <Link className="py-2 font-bold flex items-center justify-center self-stretch gap-5 p-3 duration-500 hover:bg-[#02568A] hover:cursor-pointer" href={`/transferencias/${params.id}`}>Transferir</Link>
         </div>
     );
 }
