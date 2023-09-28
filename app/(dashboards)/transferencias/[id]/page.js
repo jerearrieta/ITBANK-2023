b8ff04dadc44a4cc930c459ea200565b9b445887
Gabriel Checchi <checchigab@gmail.com>
2023-09-27 21:59:46 -0300

    Implementar transferencias

--- app/(dashboards)/transferencias/[id]/page.js
+++ app/(dashboards)/transferencias/[id]/page.js
@@ -1,10 +1,13 @@
+import { createClient } from "@supabase/supabase-js";
+
 import TransactionForm from "./TransactionForm";
 
 // dynamic = "force-dynamic" fuerza a que el componente/pagina sea generado de forma dinamica.
 export const dynamic = "force-dynamic";
 
 export default async function Page({ params }) {
-    return (
-        <TransactionForm />
-    );
+    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+    const { data } = await supabase.from("users").select("first_name, last_name").eq("id", params.id).single();
+
+    return <TransactionForm id={params.id} first_name={data.first_name} last_name={data.last_name}/>;
 }
