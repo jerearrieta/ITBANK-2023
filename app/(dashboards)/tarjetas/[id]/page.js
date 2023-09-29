07d5d830986215a62daf36694c1710a5f4495d06
jerearrieta <jerearrieta2254@gmail.com>
2023-09-28 21:08:48 -0300

    style tarjeta

--- app/(dashboards)/tarjetas/[id]/page.js
+++ app/(dashboards)/tarjetas/[id]/page.js
@@ -8,27 +8,40 @@ export const dynamicParams = false;
 
 // generateStaticParams es el equivalente a getStaticPaths del Pagers Router
 export async function generateStaticParams() {
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-    const { data } = await supabase.from("cards").select("id");
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  const { data } = await supabase.from("cards").select("id");
 
-    return data.map((record) => ({
-        id: String(record.id)
-    }))
+  return data.map((record) => ({
+    id: String(record.id),
+  }));
 }
 
 export default async function CardDetail({ params }) {
-    const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
-    const { data } = await supabase.from("cards").select("number, expiration, cvc, limit").eq("id", params.id).single();
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  );
+  const { data } = await supabase
+    .from("cards")
+    .select("number, expiration, cvc, limit")
+    .eq("id", params.id)
+    .single();
 
-    return (
-        <div>
-            <p>{params.id}</p>
-            <p>{data.number}</p>
-            <p>{data.expiration}</p>
-            <p>{data.cvc}</p>
-            <p>{data.limit}</p>
-
-            <Link href={`/tarjetas/${params.id}/edit`} className="btn">Modificar limite</Link>
-        </div>
-    );
+  return (
+    <div className="flex mx-auto flex-col w-1/3 border-2 border-black p-4 rounded">
+      <div className="flex flex-col items-center mx-auto w-full">
+        <p className="py-1">{params.id}</p>
+        <p className="py-1">{data.number}</p>
+        <p className="py-1">{data.expiration}</p>
+        <p className="py-1">{data.cvc}</p>
+        <p className="py-1">{data.limit}</p>
+      </div>
+      <Link href={`/tarjetas/${params.id}/edit`} className="btn">
+        Modificar limite
+      </Link>
+    </div>
+  );
 }
