6f0a19634cc376dc089be3c5399bd43ec5c1aa45
Gabriel Checchi <checchigab@gmail.com>
2023-09-29 21:18:45 -0300

    Arreglos pagina 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,21 +1,16 @@
-import React from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 const NotFoundPage = () => {
   return (
     <div className="bg-white min-h-screen bg-gradient-to-tr from-blue-900 to-slate-700">
-      <div>
-        <img
-          className="mx-auto pt-10"
-          src="/assets/white_logo_title.webp"
-          width={500}
-        ></img>
-      </div>
+      <Image src="/assets/white_logo_title.webp" width={500} height={500} alt="Guardian Bank" title="Guardian Bank" className="mx-auto pt-10 w-auto h-auto" priority />
+
       <div className="flex justify-center flex-col ">
-        <h1 className="font-bold text-white pt-32 text-3xl mx-auto">
+        <h1 className="font-bold text-white pt-32 text-3xl mx-auto text-center">
           Upss, No pudimos encontrar esta pagina
         </h1>
-        <p className="font-bold text-white mx-auto text-xl pt-10">
+        <p className="font-bold text-white mx-auto text-xl pt-10 text-center">
           Hemos buscado por todos lados, pero no hemos conseguido encontrar el sitio
           que estas buscando.
         </p>
