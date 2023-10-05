1dea51f35a00ccf3d2ff0ccfde49ca7e5272345f
Gabriel Checchi <checchigab@gmail.com>
2023-10-05 20:35:35 -0300

    Arreglos

--- listado_cheques.py
+++ listado_cheques.py
@@ -2,7 +2,7 @@ from argparse import ArgumentParser, FileType, ArgumentTypeError
 from csv import reader, writer
 from datetime import datetime
 from io import TextIOWrapper
-from os import system
+import os
 
 
 def imprimir_espaciado(elems: list[str], espacios: list[int], separador=" "):
@@ -42,7 +42,7 @@ def evaluar_cheque(dni_cheque: str, tipo_cheque: str, estado_cheque: str, fecha_
 
 	return resultado
 
-def filtrar_cheques(archivo: TextIOWrapper, salida: str, dni: str, tipo_cheque: str, estado_cheque: str = None, fecha: list[datetime] = None):
+def filtrar_cheques(archivo: TextIOWrapper, salida: str, dni_filtro: str, tipo_filtro: str, estado_filtro: str = None, fecha_filtro: list[datetime] = None):
 	"""
 	Filtra los cheques presentes en `archivo` y muestra los resultados por pantalla si `salida` es "pantalla" o mediante un archivo CSV si `salida` es "csv"
 
@@ -50,13 +50,13 @@ def filtrar_cheques(archivo: TextIOWrapper, salida: str, dni: str, tipo_cheque: 
 	"""
 	
 	salida = salida.lower()
-	cheques_filtrados = []
+	cheques_filtrados = set()
 	archivo_salida_csv, csv_writer = None, None
 	espacios = (11, 13, 16, 20, 21, 10, 20, 20, 10, 10, 0)
 
 	cheques = reader(archivo, delimiter=';')
 	encabezado = next(cheques)
-	
+
 	# Si `salida` es CSV, entonces crear y abrir el archivo de salida en modo escritura y escribir el encabezado. De lo contrario, solo imprimir el encabezado
 	if salida == "csv":
 		archivo_salida_csv = open(f"{dni}_{datetime.now().timestamp()}", "w", newline="")
@@ -68,25 +68,30 @@ def filtrar_cheques(archivo: TextIOWrapper, salida: str, dni: str, tipo_cheque: 
 
 	# Leemos el CSV linea por linea para evitar cargarlas todas juntas, lo cual podria sobrecargar la memoria en caso de que haya una excesiva cantidad de lineas
 	for cheque in cheques:
-		# Convertimos las fechas de timestamp a datetime
-		cheque[6] = datetime.fromtimestamp(int(cheque[6]))
-		cheque[7] = datetime.fromtimestamp(int(cheque[7]))
+		if len(cheque) == 0:
+			continue
+			
+		nro_cheque = cheque[0]
+		dni_cheque = cheque[8]
+		tipo_cheque = cheque[9]
+		estado_cheque = cheque[10]
+		fecha_cheque = datetime.fromtimestamp(int(cheque[7]))
 
 		# Evaluamos cada cheque para saber si debe ser descartado o no segun los filtros ingresados
-		if evaluar_cheque(cheque[8], cheque[9], cheque[10], cheque[7], dni, tipo_cheque, estado_cheque, fecha): 
-			# Verificamos que no haya cheques con numeros repetidos
-			if cheque[0] in cheques_filtrados:
-				system("cls")
-				raise ValueError(f"Se encontraron dos cheques con el mismo numero ({cheque[0]}) bajo el DNI {dni}.")
+		if evaluar_cheque(dni_cheque, tipo_cheque, estado_cheque, fecha_cheque, dni_filtro, tipo_filtro, estado_filtro, fecha_filtro): 
+			# Verificamos que no haya cheques con numeros repetidos, en cuyo caso 
+			if nro_cheque in cheques_filtrados:
+				os.system('cls' if os.name == 'nt' else 'clear')
+				raise ValueError(f"Se encontraron dos cheques con el mismo numero ({nro_cheque}) bajo el DNI {dni_filtro}.")
 			
-			cheques_filtrados.append(cheque[0])
-
-			# Convertimos los datetime a su representacion en string para mostrarlos
-			cheque[6] = str(cheque[6])
-			cheque[7] = str(cheque[7])
-
+			cheques_filtrados.add(nro_cheque)
+			
 			# Mostramos/guardamos los datos segun `salida`
 			if salida == "pantalla":
+				# Convertimos los timestamp a su representacion en string para mostrarlos
+				cheque[6] = str(datetime.fromtimestamp(int(cheque[6])))
+				cheque[7] = str(fecha_cheque)
+
 				imprimir_espaciado(cheque, espacios)
 
 			else:
@@ -98,14 +103,14 @@ def filtrar_cheques(archivo: TextIOWrapper, salida: str, dni: str, tipo_cheque: 
 		archivo_salida_csv.close()
 
 def dni(value: str):
-	if not value.isdigit() or len(value) != 8:
+	if not value.isdigit() or not 7 <= len(value) <= 8:
 		raise ValueError()
 
 	return value
 
 def date(value):
 	try:
-		return datetime.strptime(value, "%d/%m/%Y").date()
+		return datetime.strptime(value, "%d/%m/%Y")
 
 	except ValueError:
 		raise ArgumentTypeError(f"invalid date value: '{value}'. Make sure the format is dd/mm/yyyy")
@@ -116,16 +121,13 @@ if __name__ == "__main__":
 
 	# Definimos todos los parametros que el script va a poder recibir por consola, indicando el formato para cada uno
 	parser.add_argument("archivo", type=FileType("r"), metavar="ruta_archivo", help="La ruta del archivo CSV conteniendo los cheques. Puede ser absoluta o relativa.")
-	parser.add_argument("dni", type=dni, metavar="DNI", help="DNI a filtrar")
+	parser.add_argument("dni_filtro", type=dni, metavar="DNI", help="DNI a filtrar")
 	parser.add_argument("salida", choices=["PANTALLA", "CSV", "pantalla", "csv"], metavar="salida", help="Si el valor es 'PANTALLA', se informaran los resultados mediante pantalla. Si el valor es 'CSV', se informaran los resultados mediante un archivo CSV.")
-	parser.add_argument("tipo_cheque", choices=["EMITIDO", "DEPOSITADO", "emitido", "depositado"], metavar="tipo_cheque", help="Puede ser 'EMITIDO' o 'DEPOSITADO'")
-	parser.add_argument("estado_cheque", nargs="?", choices=["APROBADO", "PENDIENTE", "RECHAZADO", "aprobado", "pendiente", "rechazado"], metavar="estado_cheque", help="Puede ser 'APROBADO', 'PENDIENTE' o 'RECHAZADO'")
-	parser.add_argument("--fecha", nargs=2, type=date, metavar="fecha", help="Rango de fechas a filtrar en el formato dd/mm/yyyy")
+	parser.add_argument("tipo_filtro", choices=["EMITIDO", "DEPOSITADO", "emitido", "depositado"], metavar="tipo_cheque", help="Puede ser 'EMITIDO' o 'DEPOSITADO'")
+	parser.add_argument("estado_filtro", nargs="?", choices=["APROBADO", "PENDIENTE", "RECHAZADO", "aprobado", "pendiente", "rechazado"], metavar="estado_cheque", help="Puede ser 'APROBADO', 'PENDIENTE' o 'RECHAZADO'")
+	parser.add_argument("--fecha_filtro", nargs=2, type=date, metavar="fecha", help="Rango de fechas a filtrar en el formato dd/mm/yyyy")
 
 	# Parseamos los parametros ingresados por consola y llamamos a filtrar_cheques para el filtrado
 	args = parser.parse_args()
-
-	#args = parser.parse_args(["cheques.csv", "12345678", "PANTALLA", "EMITIDO", "--fecha", "10/12/2020", "10/12/2020"])
-	#args = parser.parse_args(["cheques.csv", "12345678", "CSV", "EMITIDO"])
 	
 	filtrar_cheques(**args.__dict__)
