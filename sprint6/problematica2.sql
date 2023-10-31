202f58eaecfd1d71d1d37b01cf1c7f926e3c3dcb
Gabriel Checchi <checchigab@gmail.com>
2023-10-31 16:04:36 -0300

    Segunda problematica

--- sprint6/problematica2.sql
+++ sprint6/problematica2.sql
@@ -13,16 +13,84 @@ Mostrar todos los clientes que se llaman “Anne” o “Tyler” ordenados
 por edad de menor a mayor.
 */
 
+CREATE VIEW IF NOT EXISTS vista_cliente(id, nombre, apellido, DNI, edad, numero_sucursal) AS
+SELECT
+    customer_id,
+    customer_name,
+    customer_surname,
+    customer_DNI,
+    case
+        when STRFTIME('%m-%d', DATE()) < STRFTIME('%m-%d', dob) 
+            then DATE() - dob - 1
+        else DATE() - dob
+    end,
+    branch_id
+FROM cliente;
 
+SELECT * 
+FROM vista_cliente
+WHERE edad > 40
+ORDER BY DNI ASC;
+
+SELECT * 
+FROM vista_cliente
+WHERE nombre IN ('Anne', 'Tyler')
+ORDER BY edad ASC;
 
 /*
 Segundo punto
 
-Dado el siguiente JSON. Insertar 5 nuevos clientes en la base de
+Dado el siguiente JSON insertar 5 nuevos clientes en la base de
 datos y verificar que se haya realizado con éxito la inserción.
 */
 
+INSERT INTO cliente (customer_name, customer_surname, customer_DNI, dob, branch_id)
+SELECT 
+    json_extract(value, '$.customer_name') customer_name,
+    json_extract(value, '$.customer_surname') customer_surname,
+    json_extract(value, '$.customer_DNI') customer_DNI,
+    json_extract(value, '$.customer_dob') customer_dob,
+    json_extract(value, '$.branch_id') branch_id
+FROM json_each('
+[
+    {
+    "customer_name": "Lois",
+    "customer_surname": "Stout",
+    "customer_DNI": 47730534,
+    "branch_id": 80,
+    "customer_dob": "1984-07-07"
+    },
+    {
+    "customer_name": "Hall",
+    "customer_surname": "Mcconnell",
+    "customer_DNI": 52055464,
+    "branch_id": 45,
+    "customer_dob": "1968-04-30"
+    },
+    {
+    "customer_name": "Hilel",
+    "customer_surname": "Mclean",
+    "customer_DNI": 43625213,
+    "branch_id": 77,
+    "customer_dob": "1993-03-28"
+    },
+    {
+    "customer_name": "Jin",
+    "customer_surname": "Cooley",
+    "customer_DNI": 21207908,
+    "branch_id": 96,
+    "customer_dob": "1959-08-24"
+    },
+    {
+    "customer_name": "Gabriel",
+    "customer_surname": "Harmon",
+    "customer_DNI": 57063950,
+    "branch_id": 27,
+    "customer_dob": "1976-04-01"
+    }
+]');
 
+SELECT * FROM cliente ORDER BY customer_id DESC LIMIT 5;
 
 /*
 Tercer punto
@@ -31,7 +99,11 @@ Actualizar 5 clientes recientemente agregados en la base de datos dado que hubo
 un error en el JSON que traía la información, la sucursal de todos es la 10.
 */
 
+UPDATE cliente
+SET branch_id = 10
+WHERE customer_id IN (SELECT customer_id FROM cliente ORDER BY customer_id DESC LIMIT 5);
 
+SELECT * FROM cliente ORDER BY customer_id DESC LIMIT 5;
 
 /*
 Cuarto punto
@@ -39,7 +111,8 @@ Cuarto punto
 Eliminar el registro correspondiente a “Noel David” realizando la selección por el nombre y apellido.
 */
 
-
+-- No hay ningun Noel David
+DELETE FROM cliente WHERE customer_name = 'Noel' AND customer_surname = 'David';
 
 /*
 Quinto punto
@@ -47,6 +120,9 @@ Quinto punto
 Consultar sobre cuál es el tipo de préstamo de mayor importe.
 */
 
+-- Dos posibles soluciones
+SELECT loan_type from prestamo WHERE loan_total = (SELECT MAX(loan_total) FROM prestamo);
 
+SELECT loan_type from prestamo ORDER BY loan_total DESC LIMIT 1;
 
 -- COMMIT;
