e73d2a6eca1b03b6178c1d13b0f13b8592590639
Gabriel Checchi <checchigab@gmail.com>
2023-10-30 23:51:51 -0300

    Continuar primera problematica

--- sprint6/pruebas.sql
+++ sprint6/pruebas.sql
@@ -2,4 +2,6 @@
 
 -- SELECT substr('08/06/2014', 7, 4) || '-' || substr('08/06/2014', 4, 2) || '-' || substr('08/06/2014', 1, 2);
 
-SELECT * from cliente;
+-- SELECT * from cliente;
+
+SELECT ABS(RANDOM()) % 3 + 1;
