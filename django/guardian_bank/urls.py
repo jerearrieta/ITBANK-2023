5bbc37be5778e904ba2f2a7acafe9947210583ef
Gabriel Checchi <checchigab@gmail.com>
2023-11-09 16:27:29 -0300

    Layout base

--- django/guardian_bank/urls.py
+++ django/guardian_bank/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from base.views import home
 
 urlpatterns = [
     path('', include('autenticacion.urls')),
+    path('home/', home),
     path('admin/', admin.site.urls),
 ]
