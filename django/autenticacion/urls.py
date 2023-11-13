ef9d5d6fd78adbf77e7dd2ae2f8047d6acfd22b2
Gabriel Checchi <checchigab@gmail.com>
2023-11-13 02:10:25 -0300

    Cerrar sesion

--- django/autenticacion/urls.py
+++ django/autenticacion/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.inicio_sesion, name='login'),
     path('registro/', views.registro, name='registro'),
+    path('cerrar-sesion/', views.cerrar_sesion, name='logout'),
 ]
