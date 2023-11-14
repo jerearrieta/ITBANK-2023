322e0d9ced06e0be6ddd6fc3a5992d104dabfff6
Gabriel Checchi <checchigab@gmail.com>
2023-11-14 14:51:02 -0300

    Pagina home

--- django/base/views.py
+++ django/base/views.py
@@ -4,4 +4,10 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def home(req):
-	return render(req, "base/home.html")
+	user = req.user
+
+	context = {
+		"full_name": f"{user.first_name} {user.last_name}"
+	}
+
+	return render(req, "base/home.html", context)
