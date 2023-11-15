f884168472cd03fde16fe507d7471016c28e9b8a
Gabriel Checchi <checchigab@gmail.com>
2023-11-15 14:48:16 -0300

    Arreglar urls

--- django/cuentas/urls.py
+++ django/cuentas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cuenta, name='listar_cuentas'),
+    path('', views.cuenta, name='cuentas'),
 ]
--- django/tarjetas/urls.py
+++ django/tarjetas/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.tarjeta, name='listar_tarjetas'),
+    path('', views.tarjeta, name='tarjetas'),
 ]
