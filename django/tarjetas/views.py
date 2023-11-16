a337871a3170c45f6dbc95042af623a165001f29
Gabriel Checchi <checchigab@gmail.com>
2023-11-16 19:07:57 -0300

    Empezar pagina de tarjetas

--- django/tarjetas/views.py
+++ django/tarjetas/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def tarjeta(req):
-	return render(req, "tarjetas/tarjetas.html")
+	cliente = req.user.cliente
+
+	context = {
+		#"tarjetas_debito": cliente.tarjeta_set.all(),
+		#"tarjetas_credito": cliente.tarjeta_set.all(),
+	}
+
+	return render(req, "tarjetas/tarjetas.html", context)
