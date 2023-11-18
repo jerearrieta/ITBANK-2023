00c192e7bc4b789b8558ede121abe6a23a524ea9
jerearrieta <jerearrieta2254@gmail.com>
2023-11-18 18:07:47 -0300

    transferencias

--- django/movimientos/urls.py
+++ django/movimientos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.movimientos, name='movimientos'),
-    path('<int:id>', views.movimientos_cuenta, name='movimientos_cuenta'),
+    # path('<int:id>', views.movimientos_cuenta, name='movimientos_cuenta'),
     path('convertidor/', views.convertidor, name='convertidor'),
+    path('exito_transferencia/', views.exito_transferencia, name='exito_transferencia'),
 ]
--- django/movimientos/views.py
+++ django/movimientos/views.py
@@ -1,16 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from .forms import TransferenciaForm
+from .models import Movimiento
+from cuentas.models import Cuenta 
 
 # Create your views here.
 @login_required
 def movimientos(req):
-    return render(req, 'movimientos/transferencias.html')
+    cliente = req.user.cliente
 
+    # get_object_or_404 para manejar automáticamente el caso de cuenta no encontrada
+    cuenta_origen = get_object_or_404(Cuenta, cliente=cliente)
+
+    if req.method == 'POST':
+        form = TransferenciaForm(req.POST)
+        if form.is_valid():
+            monto = form.cleaned_data['monto']
+            razon = form.cleaned_data['razon']
+
+            Movimiento.objects.create(cuenta=cuenta_origen, tipo_operacion=razon, monto=monto)
+            
+
+            return redirect('exito_transferencia')
+    
+    else:
+        form = TransferenciaForm()
+
+    return render(req, 'movimientos/transferencias.html', {'form': form})
 
 @login_required
-def movimientos_cuenta(req, id):
-    return render(req, 'movimientos/transferencias.html')
+def exito_transferencia(req):
+    return render(req, 'movimientos/exito_transferencia.html')
+
+
+# @login_required
+# def movimientos_cuenta(req, id):
+#     return render(req, 'movimientos/transferencias.html')
 
 
 @login_required
