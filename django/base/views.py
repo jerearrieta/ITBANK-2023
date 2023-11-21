95b080df7e6d6095249771bce6fa8b93f90353ea
jerearrieta <jerearrieta2254@gmail.com>
2023-11-21 01:24:53 -0300

    actividad reciente

--- django/base/views.py
+++ django/base/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from movimientos.models import Movimiento
 
 
 # Create your views here.
 @login_required
 def home(req):
-	user = req.user
+    user = req.user
 
-	context = {
-		"full_name": f"{user.first_name} {user.last_name}"
-	}
+    movimientos = Movimiento.objects.filter(cuenta__cliente=user.cliente)
 
-	return render(req, "base/home.html", context)
+    context = {
+        "full_name": f"{user.first_name} {user.last_name}",
+        "movimientos": movimientos,
+    }
+
+    return render(req, "base/home.html", context)
