a9a66ee763f6d28b1abba0fd0098358d1550675d
Camila González <camgonzalez36@gmail.com>
2023-11-24 21:45:15 -0300

    arreglos

--- django/empleados/views.py
+++ django/empleados/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# Obtener datos de un cliente : retrieve detail
+
+# Obtener saldo de cuenta de un cliente : retrieve detail
+
+# Obtener monto de prestamos de un cliente : retrieve detail
+
+# Obtener monto de prestamos de una sucursal : retrieve detail
+
+# Obtener tarjetas asociadas a un cliente : retrieve detail
+
+# Generar una solicitud de prestamos para un cliente : create
+
+# Anular solicitud de prestamo de cliente : delete
+
+# Modificar dirección de un cliente : update
+
+# Listado de todas las sucurlsales : retrieve no detail
--- django/guardian_bank/settings.py
+++ django/guardian_bank/settings.py
@@ -49,8 +49,6 @@ INSTALLED_APPS = [
     'movimientos',
     'prestamos',
     'tarjetas',
-    
-    'servicios',
 ]
 
 MIDDLEWARE = [
