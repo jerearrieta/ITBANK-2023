bf6d938863ec50955b733ba4c4b06c5df55eba6b
Camila González <camgonzalez36@gmail.com>
2023-11-25 12:32:56 -0300

    serializer

--- django/empleados/views.py
+++ django/empleados/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializer import EmpleadoSerializer
+from .models import Empleado
 
 # Create your views here.
+class EmpleadoView(viewsets.ModelViewSet):
+    serializer_class = EmpleadoSerializer
+    queryset = Empleado.objects.all()
 
 # Obtener datos de un cliente : retrieve detail
 
