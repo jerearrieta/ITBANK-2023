d47bf1262fde01256e553f330d385fbb36827bd0
Gabriel Checchi <checchigab@gmail.com>
2023-12-05 18:20:23 -0300

    Agregar PDFs de facturas

--- django/clientes/admin.py
+++ django/clientes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente, TipoCliente, DireccionCliente
 
 # Register your models here.
+admin.site.register(Cliente)
+admin.site.register(TipoCliente)
+admin.site.register(DireccionCliente)
+
--- django/facturas/admin.py
+++ django/facturas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Factura
 
 # Register your models here.
+
+admin.site.register(Factura)
